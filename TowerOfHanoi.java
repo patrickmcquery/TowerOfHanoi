ab0574d66137531554c6bf0191c604a0b2e09379
Patrick McQuery <patrickmcquery@gmail.com>
2023-02-28 15:19:19 -0800

    Comments Expanded functionality to any number of disks

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,3 +1,9 @@
+/*
+ * Recursive Tower of Hanoi solver for any number of disks.
+ * Patrick McQuery
+ */
+import java.util.Scanner;
+
 public class TowerOfHanoi
 {
     static int[][] towers;
@@ -5,13 +11,19 @@ public class TowerOfHanoi
     static int step = 1;
     public static void main(String args[])
     {
-        disks = 4;
+        System.out.println("How many disks would you like to solve for? (I would recommend between 1 and 12)");
+        Scanner console = new Scanner(System.in);
+        disks = console.nextInt();
         towers = new int[3][disks];
         setup();
         print();
         solve(disks, 0, 2, 1);
     }
-
+    /*
+     * The starting value of "n" is the number of disks, "from" is the tower that the disk is on currently,
+     * "to" is the tower that it will be moved to, and "temp" is the other tower that will not be used for the 
+     * current placement but is needed for subsequent moves.
+     */
     public static void solve(int n, int from, int to, int temp)
     {
         if (n == 0) 
@@ -25,7 +37,9 @@ public class TowerOfHanoi
         print();
         solve(n - 1, temp, to, from);
     }
-
+    /*
+     * Place searches for the specified disk (n) and places it on the "to" tower
+     */
     public static void place(int n, int to)
     {
         for(int i = disks - 1; i >= 0; i--)
@@ -47,6 +61,7 @@ public class TowerOfHanoi
             }
         }
     }
+
     public static void setup()
     {
         for(int i = 0; i < disks; i++)
@@ -54,35 +69,81 @@ public class TowerOfHanoi
             towers[0][i] = disks - i;
         }
     }
-    
+
+    /*
+     * I added an ASCII printout to help visually represent what is happening.
+     * I use a total of 3 methods to print each individual part to help break
+     * the problem up into smaller chunks.
+     */
     public static void print()
     {
         for(int i = disks; i >= 0; i--)
         {
-            if(i == disks)
+            printLayer(i);
+        }
+    }
+    public static void printLayer(int layer)
+    {
+        if(layer == disks)
+        {
+            String segment = "";
+            for(int i = 1; i <= 2 * disks - 1; i++)
             {
-                System.out.println("   |       |       |");
-            }
-            else
-            {
-                for(int j = 0; j < 3; j++)
+                if(i == ((2 * disks - 1) / 2) + 1)
                 {
-                    switch(towers[j][i])
-                    {
-                        case 0: System.out.print("   |    ");
-                                break;
-                        case 1: System.out.print("   -    ");
-                                break;
-                        case 2: System.out.print("  ---   ");
-                                break;
-                        case 3: System.out.print(" -----  ");
-                                break;
-                        case 4: System.out.print("------- ");
-                                break;
-                    }
+                    segment += "|";
                 }
-                System.out.println();
+                else
+                {
+                    segment += " ";
+                }
+            }
+            System.out.print(segment + " " + segment + " " + segment);
+        }
+        else
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                printSegment(layer, i);
             }
         }
+        System.out.println();
+    }
+    public static void printSegment(int layer, int tower)
+    {
+        String segment = "";
+        int lengthOfSegment = 2 * disks - 1;
+        if(towers[tower][layer] == 0)
+        {
+            for(int i = 1; i <= lengthOfSegment; i++)
+            {
+                if(i == (lengthOfSegment / 2) + 1)
+                {
+                    segment += "|";
+                }
+                else
+                {
+                    segment += " ";
+                }
+            }
+        }
+        else
+        {
+            int lengthOfDisk = (2 * towers[tower][layer]) - 1;
+            int start = (lengthOfSegment / 2 + 1) - (lengthOfDisk / 2);
+            for(int i = 1; i <= lengthOfSegment; i++)
+            {
+                if(i >= start && i < start + lengthOfDisk)
+                {
+                    segment += "-";
+                }
+                else
+                {
+                    segment += " ";
+                }
+            }
+        }
+        
+        System.out.print(segment + " ");
     }
 }
